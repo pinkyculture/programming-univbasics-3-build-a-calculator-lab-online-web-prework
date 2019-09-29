19b489054e9d3940af626182fd1c73caaeaacbe7
interactive-cache-8375 <randomblue1@yahoo.com>
2019-09-29 20:53:38 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -8,12 +8,12 @@ num1 - num2
 
 end
 
-def division(num1 = , num2)
-
+def division(num1 = 4, num2 = 4)
+num1 / num2
 end
 
-def multiplication(num1, num2)
-
+def multiplication(num1 = 3, num2 = 4)
+num1 * num2
 end
 
 def modulo(num1, num2)
